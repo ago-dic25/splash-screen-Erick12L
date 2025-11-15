ad6638a99797d312a57c76bf7c8b00b8b38e971a
Erick Leonardo Morales Sanchez <erick.moraless@uanl.edu.mx>
2025-11-14 22:11:04 -0600

    Act hecha en clase

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 import { useEffect } from "react";
 import * as SplashScreen from "expo-splash-screen";
 import { View, Text } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
   useEffect(() => {
     SplashScreen.preventAutoHideAsync(); // evita que se oculte el splash screen de forma predeterminada
     setTimeout(() => {
       SplashScreen.hideAsync(); // oculta después de 2s
-    }, 2000);
+    }, 4000);
   }, []);
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Aqui puedes agregar un componente de React (puedes crear un componente separado o mover el codigo creado en clase anterior e importarlo en la sección de arriba) </Text>
-    </View>
-  );
+      <LinearGradient
+        colors={["#c5cdddff", "#5c88c9ff", "#0130ffff"]} // colores del gradiente
+        style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+      >
+        <Text style={{ color: "white", textAlign: "center", marginHorizontal: 20 }}>
+          Aquí puedes agregar un componente de React (o el que creaste en clase anterior)
+        </Text>
+      </LinearGradient>
+    );
 }
